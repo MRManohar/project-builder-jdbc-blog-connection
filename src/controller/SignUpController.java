edadc6750d1a926ca198d6eb1a773c76f782047f
MRManohar <lmmrider99@gmail.com>
2020-07-28 12:32:55 +0530

    FSJ0420A109

--- src/controller/SignUpController.java
+++ src/controller/SignUpController.java
@@ -29,6 +29,18 @@ public class SignUpController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Sign up Controller");
 		// Fill your code here
+		try {
+			Connection con = null;
+			con = ConnectionManager.getConnection();
+			if(con != null)
+				System.out.println("Connection established");
+			
+		} catch (Exception e) {
+			System.out.println("Check your connection");
+		}
+		
+		
+		
 		RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/signupView.jsp");
 		rd.forward(request,response);
 	}
--- src/utility/ConnectionManager.java
+++ src/utility/ConnectionManager.java
@@ -1,4 +1,28 @@
 package utility;
 
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
 
-
+public class ConnectionManager{
+	public static Connection getConnection() throws Exception {
+		Properties prop = new Properties();
+		String driver = prop.getProperty("driver");
+		String url = prop.getProperty("url");
+		String username = prop.getProperty("username");
+		String password = prop.getProperty("password");
+		Class.forName(driver);
+		
+		Connection con = DriverManager.getConnection(url,username,password);
+		
+		return con;		
+	}
+	public static Properties loadPropertiesFile() throws Exception {
+		Properties prop = new Properties();	
+		InputStream in = ConnectionManager.class.getClassLoader().getResourceAsStream("jdbc.properties");
+		prop.load(in);
+		in.close(); 
+		return prop;
+	}
+}
